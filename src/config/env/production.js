15ceb39914e8c1db44b125008fe2fd6097412fe5
Michal Sloupenský <michal.sloupensky@previo.cz>
2019-01-21 07:32:39 +0100

    Added procfile and production env file

---
+++ src/config/env/production.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  hostname: 'http://localhost:3000',
+  firebase: {
+    apiKey: "AIzaSyBiOOw42qn5dwhoNoyM9hoj4gpUxnrD07w",
+    authDomain: "strv-addressbook-sloupensky.firebaseapp.com",
+    databaseURL: "https://strv-addressbook-sloupensky.firebaseio.com",
+    projectId: "strv-addressbook-sloupensky",
+    storageBucket: "strv-addressbook-sloupensky.appspot.com",
+    messagingSenderId: "229642698554"
+  }
+}
